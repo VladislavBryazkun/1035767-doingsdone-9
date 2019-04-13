97f422b49c1510c65b068a87980bd6a28526a66f
Vladsilav <vladbryazkun@gmail.com>
2019-04-13 19:05:06 +0300

    УсловияПравка1

--- index.php
+++ index.php
@@ -94,7 +94,7 @@ $show_complete_tasks = rand(0, 1);
                         <td class="task__date"></td>
                     </tr>
                     <!--показывать следующий тег <tr/>, если переменная $show_complete_tasks равна единице-->
-                    <?php if ($show_complete_tasks == 1): ?>checked<?php 
+                    <?php if ($show_complete_tasks == 1): ?> 
                          <tr class="tasks__item task task--completed">
                             <td class="task__select">
                                 <label class="checkbox task__checkbox">
@@ -105,7 +105,7 @@ $show_complete_tasks = rand(0, 1);
                             <td class="task__date">10.10.2019</td>
                             <td class="task__controls"></td>
                         </tr>
-                    endif; ?>
+                   <?php endif; ?>
                 </table>
             </main>
         </div>
