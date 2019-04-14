f8e26f89952334187cbe7da17ba601124fdb95c1
Vladsilav <vladbryazkun@gmail.com>
2019-04-14 17:29:25 +0300

    ПравкиУсловия2

--- index.php
+++ index.php
@@ -28,8 +28,7 @@ $show_complete_tasks = rand(0, 1);
 
                 <div class="main-header__side-item user-menu">
                     <div class="user-menu__data">
-                        <p>Владислав</p>
-
+                        <p>Константин</p>
                         <a href="#">Выйти</a>
                     </div>
                 </div>
@@ -72,8 +71,7 @@ $show_complete_tasks = rand(0, 1);
 
                     <label class="checkbox">
                         <!--добавить сюда аттрибут "checked", если переменная $show_complete_tasks равна единице-->
-                        <?php if ($show_complete_tasks == 1): ?>checked<?php endif; ?>
-                        <input class="checkbox__input visually-hidden show_completed" <?php if ($show_complete_tasks == 1): ?>checked<?php endif; ?> type="checkbox">
+                     <input class="checkbox__input visually-hidden show_completed" <?php if ($show_complete_tasks === 1): ?>checked<?php endif; ?> type="checkbox">
                         <span class="checkbox__text">Показывать выполненные</span>
                     </label>
                 </div>
@@ -94,7 +92,7 @@ $show_complete_tasks = rand(0, 1);
                         <td class="task__date"></td>
                     </tr>
                     <!--показывать следующий тег <tr/>, если переменная $show_complete_tasks равна единице-->
-                    <?php if ($show_complete_tasks == 1): ?> 
+                    <?php if ($show_complete_tasks === 1): ?> 
                          <tr class="tasks__item task task--completed">
                             <td class="task__select">
                                 <label class="checkbox task__checkbox">
