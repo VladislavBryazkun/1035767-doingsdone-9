5f31d4dcd1e061d01c6db076e083ffcc2bcaba8c
Vladsilav <vladbryazkun@gmail.com>
2019-04-28 18:31:37 +0300

    '3-3-1'

---
+++ function.php
@@ -0,0 +1,9 @@
+﻿<?php
+    function countTasks($tasks, $task_name)
+    {
+        $category_task = array_filter($tasks, function ($n) use ($task_name) {
+            return $n['category'] == $task_name;
+        }
+        );
+        return count($category_task);
+    }
