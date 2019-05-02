f3655fa52e3a7d032398f9f3ddc9b004550cff6b
Vladsilav <vladbryazkun@gmail.com>
2019-05-02 19:33:40 +0300

    [3-2-1Даты

--- function.php
+++ function.php
@@ -1,9 +1,24 @@
-﻿<?php
-    function countTasks($tasks, $task_name)
-    {
-        $category_task = array_filter($tasks, function ($n) use ($task_name) {
-            return $n['category'] == $task_name;
+﻿    <?php
+        function countTasks($tasks, $task_name)
+        {
+            $category_task = array_filter($tasks, function ($n) use ($task_name) {
+                return $n['category'] == $task_name;
+            }
+            );
+            return count($category_task);
         }
-        );
-        return count($category_task);
+        ;
+
+    function date_work($date)
+    {
+        $ts = time();
+        $task_date = strtotime($date);
+        $ts_diff = $task_date - $ts;
+        $sec_in_hour = 3600;
+        if (($ts_diff / $sec_in_hour) <= 24 && $date != 'Нет') {
+            return ("task--important");
+        } else {
+            return ('');
+        }
     }
+    ;
