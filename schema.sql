eff4ba1b53f2865e950a954a9d445dd1a7d7670c
Vladsilav <vladbryazkun@gmail.com>
2019-05-10 14:02:41 +0300

    [4-1-1Правки по замечаниям]

--- schema.sql
+++ schema.sql
@@ -5,27 +5,28 @@ CREATE DATABASE justdoit
 
 CREATE TABLE projects (
         id INT AUTO_INCREMENT PRIMARY KEY,
-        name CHAR(50) );
+        name VARCHAR(128)
+        );
 
     CREATE TABLE tasks (
         id INT AUTO_INCREMENT PRIMARY KEY,
-        task CHAR(100),
-        category CHAR(100),
+        task VARCHAR(128),
+        category VARCHAR(128),
         status TINYINT DEFAULT 0,
         dt_add TIMESTAMP DEFAULT  CURRENT_TIMESTAMP,
-        name CHAR(50) NOT NULL
-        );
+        name VARCHAR(128) NOT NULL,
+         );
 
     CREATE TABLE users (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        dt_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        email CHAR(50) NOT NULL,
-        name CHAR(50) NOT NULL,
-        password CHAR(20) NOT NULL
-    );
+        id          INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        date_create TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        email       VARCHAR(128) NOT NULL UNIQUE,
+        name   VARCHAR(128) NOT NULL,
+        password    VARCHAR(128) NOT NULL,
+        INDEX user_email (email)
+        );
 
-        CREATE INDEX project_name ON projects(name);
-        CREATE INDEX status ON tasks(status);
-        CREATE INDEX task_name ON tasks(name);
-        CREATE UNIQUE INDEX email ON users(email);
-        CREATE INDEX user_name ON users(name);
+            CREATE INDEX project_name ON projects(name);
+            CREATE INDEX status ON tasks(status);
+            CREATE INDEX task_name ON tasks(name);
+            CREATE INDEX user_name ON users(name);
